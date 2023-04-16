0e8ffcbda108f3382a7c61a2831831dd9a78b2a9
Hewerton <hewertonlourenco@gmail.com>
2023-04-15 21:03:44 -0300

    Feat: Make the create area design

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,29 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import './index.css'
 const Main = () => {
+const [title, setTitle] = useState('');
+const [content, setContent] = useState('');
 
+    const handleClick = () => {
+        console.log('clicou');
+    }
 
     return (
-        <div className={"container"}>
-            <header className={"header"}>
-                <h1>CodeLeap Network</h1>
-            </header>
-            <div className={"newNote"}>
-                <h1>What's on your mind?</h1>
+      <div className={"container"}>
+        <header className={"header"}>
+          <h1>CodeLeap Network</h1>
+        </header>
+        <div className={"newNote"}>
+          <h1>What's on your mind?</h1>
 
-                <div className="flex-col">
-                    <label className={"flex-col"}>
-                        Title
-                        <input type={"text"} placeholder={"Hello world"}/>
-                    </label>
-                    <label className={"flex-col"}>
-                        Content
-                        <textarea placeholder={"Content here"}/>
-                    </label>
-                    <button>Create</button>
-                </div>
-            </div>
+          <div className={"container-input"}>
+            <label className={"flex-col"}>
+              Title
+              <input
+                onChange={(e) => setTitle(e.target.value)}
+                type={"text"}
+                placeholder={"Hello world"}
+              />
+            </label>
+            <label className={"flex-col"}>
+              Content
+              <textarea
+                onChange={(e) => setContent(e.target.value)}
+                placeholder={"Content here"}
+              />
+            </label>
+
+            {(title && content !== "") ? (
+              <button onClick={handleClick}>Create</button>
+            )
+            :
+            (
+              <button onClick={handleClick} className={"deactived"} disabled>
+                Create
+              </button>
+            )}
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 export default Main;
